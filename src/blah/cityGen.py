ab8ce5b57c6d0470c8f52cc6d9c95a009b05f66f
Pierce Robson <piercerobson@gmail.com>
2019-11-21 12:00:25 -0500

    Remove some files

--- src/blah/cityGen.py
+++
@@ -1,9 +0,0 @@
-start = "<createCity name=\"city"
-x = "\" x=\""
-y = "\" y=\""
-end = "\" radius="0" color="black"/>"
-
-i = 0
-for (i = -128; i <= 1152; i+=8):
-    for (j = -128; j <= 1152; j+=8):
-        
